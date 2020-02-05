3ce2531c9477624e80ec014dc7c95660c2231006
lizz <lizz@sensetime.com>
2020-02-05 12:09:28 +0800

    fix

--- benchmarks/run_bbox_clip.py
+++ benchmarks/run_bbox_clip.py
@@ -22,7 +22,7 @@ def run_fast():
     cmin[0::2] = img_shape[1] - 1
     cmin[1::2] = img_shape[0] - 1
     clipped_bboxes = np.maximum(np.minimum(bboxes, cmin), 0)
-    check(clipped_bboxes)
+    return clipped_bboxes
 
 
 def run_clip():
@@ -30,11 +30,11 @@ def run_clip():
     cmin[0::2] = img_shape[1] - 1
     cmin[1::2] = img_shape[0] - 1
     clipped_bboxes = np.clip(bboxes, 0, cmin)
-    check(clipped_bboxes)
+    return clipped_bboxes
 
 
 def run_slow():
     clipped_bboxes = np.empty_like(bboxes, dtype=bboxes.dtype)
     clipped_bboxes[..., 0::2] = np.maximum(np.minimum(bboxes[..., 0::2], img_shape[1] - 1), 0)
     clipped_bboxes[..., 1::2] = np.maximum(np.minimum(bboxes[..., 1::2], img_shape[0] - 1), 0)
-    check(clipped_bboxes)
+    return clipped_bboxes
