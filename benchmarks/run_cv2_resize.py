0590c84943f5bf5fff280248a126ce2401479255
lizz <lizz@sensetime.com>
2020-03-18 21:25:58 +0800

    they are same
    Signed-off-by: lizz <lizz@sensetime.com>

--- benchmarks/run_cv2_resize.py
+++ benchmarks/run_cv2_resize.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-from .consts import jpg_file_256
+from .consts import jpg_file_big
 
-imgs = cv2.imread(jpg_file_256)
+imgs = cv2.imread(jpg_file_big)
 
 
 def check(img):
-    img == (256, 341, 3)
+    img == (600, 800, 3)
 
 
 def run_resize():
-    rimgs = cv2.resize(imgs, (341, 256), interpolation=cv2.INTER_LINEAR)
+    rimgs = cv2.resize(imgs, (800, 600), interpolation=cv2.INTER_LINEAR)
     return rimgs
 
 
