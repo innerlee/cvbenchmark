81fb725532e685c93dbb6efe5ddaec7122bda1b1
lizz <lizz@sensetime.com>
2020-03-24 20:49:04 +0800

    turbo is faster
    Signed-off-by: lizz <lizz@sensetime.com>

--- benchmarks/run_cv2_resize.py
+++ benchmarks/run_cv2_resize.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 from .consts import jpg_file_big
 
---
+++ benchmarks/run_read_crop.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+from turbojpeg import TJCS_RGB, TurboJPEG
+
+from .consts import jpg_file
+
+jpeg = TurboJPEG()
+
+
+def check(img):
+    assert isinstance(img, np.ndarray)
+    assert img.shape == (200, 200, 3)
+    assert img.dtype == np.uint8
+
+
+def run_turbojpeg():
+    with open(jpg_file, 'rb') as in_file:
+        img = jpeg.decode(in_file.read(), pixel_format=TJCS_RGB)
+    return img[40:240, 20:220]
+
+
+def run_PIL():
+    img = Image.open(jpg_file)
+    img = img.crop((20, 40, 220, 240))
+
+    return np.array(img)
