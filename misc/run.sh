6516ba5085456c2eb702dffadc1f591337be115c
lizz <lizz@sensetime.com>
2020-05-08 13:11:57 +0800

    ha
    Signed-off-by: lizz <lizz@sensetime.com>

--- misc/run.sh
+++ misc/run.sh
@@ -1,1 +1,2 @@
 python misc/decord_speed.py 2>/dev/null | grep init: | awk '{ print $2 }' | datamash -H mean 1 sstdev 1
+python misc/decord_speed.py 2>&1 | grep avformat_find_stream_info | awk '{ print $7 }' | sed 's/us//' | datamash -H mean 1 sstdev 1
